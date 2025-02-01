7943c2ced4472a34ee95e7ce34f6d3e18e415225
vevego <veera.ruotsalainen@metropolia.fi>
2025-02-01 19:30:51 +0200

    tietokantaa muokattu, modeleita työstetty eteenpäin

--- src/main/java/models/Assignment.java
+++ src/main/java/models/Assignment.java
@@ -1,4 +1,42 @@
 package models;
+import java.time.LocalDate;
 
 public class Assignment {
+    private final String courseName;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private String status;
+
+    public Assignment(String courseName, String title, String description, LocalDate dueDate, String status) {
+        this.courseName = courseName;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getDetails() {
+        return "Title: " + title + "\nCourse: " + courseName + "\nDescription: " + description + "\nDue Date: " + dueDate + "\nStatus: " + status;
+    }
 }
--- src/main/resources/database_creation.sql
+++ src/main/resources/database_creation.sql
@@ -1,53 +1,74 @@
-IF EXISTS DROP DATABASE study_planner;
-CREATE DATABASE study_planner;
-USE study_planner;
+IF
+EXISTS DROP
+DATABASE study_planner;
+CREATE
+DATABASE study_planner;
+USE
+study_planner;
 
 
-CREATE TABLE course (
-                        course_name VARCHAR(50) PRIMARY KEY,
-                        start_date DATE,
-                        end_date DATE
+CREATE TABLE course
+(
+    course_name VARCHAR(50) PRIMARY KEY,
+    start_date  DATE,
+    end_date    DATE,
+    instructor  VARCHAR(50)
 );
 
-CREATE TABLE study_sessions (
-                                session_id INT AUTO_INCREMENT PRIMARY KEY,
-                                course_name VARCHAR(50),
-                                title VARCHAR(50),
-                                description TEXT,
-                                start_time DATETIME,
-                                end_time DATETIME,
-                                FOREIGN KEY (course_name) REFERENCES course(course_name)
+CREATE TABLE study_session
+(
+    session_id  INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(50),
+    title       VARCHAR(50),
+    description VARCHAR(100),
+    start_time  DATETIME,
+    end_time    DATETIME,
+    FOREIGN KEY (course_name) REFERENCES course (course_name)
 );
 
-CREATE TABLE assignments (
-                             assignment_id INT AUTO_INCREMENT PRIMARY KEY,
-                             course_name VARCHAR(50),
-                             title VARCHAR(50),
-                             description TEXT,
-                             due_date DATETIME,
-                             status VARCHAR(20),
-                             FOREIGN KEY (course_name) REFERENCES course(course_name)
+CREATE TABLE assignment
+(
+    assignment_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_name   VARCHAR(50),
+    title         VARCHAR(50),
+    description   VARCHAR(100),
+    due_date      DATETIME,
+    status        VARCHAR(20),
+    FOREIGN KEY (course_name) REFERENCES course (course_name)
 );
 
-CREATE TABLE exams (
-                       exam_id INT AUTO_INCREMENT PRIMARY KEY,
-                       course_name VARCHAR(50),
-                       exam_date DATETIME,
-                       description TEXT,
-                       FOREIGN KEY (course_name) REFERENCES course(course_name)
+CREATE TABLE exam
+(
+    exam_id     INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(50),
+    exam_date   DATETIME,
+    title       VARCHAR(50),
+    description VARCHAR(100),
+    location    VARCHAR(15),
+    FOREIGN KEY (course_name) REFERENCES course (course_name)
 );
 
-CREATE TABLE class_schedule (
-                                class_id INT AUTO_INCREMENT PRIMARY KEY,
-                                course_name VARCHAR(50),
-                                location VARCHAR(15),
-                                start_time DATETIME,
-                                end_time DATETIME,
-                                day_of_week ENUM('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'),
-                                FOREIGN KEY (course_name) REFERENCES course(course_name)
+CREATE TABLE class_schedule
+(
+    class_id    INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(50),
+    location    VARCHAR(15),
+    start_time  DATETIME,
+    end_time    DATETIME,
+    day_of_week ENUM('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'),
+    FOREIGN KEY (course_name) REFERENCES course (course_name)
 );
 
-IF EXISTS DROP USER 'student_test'@'localhost';
-CREATE USER 'student_test'@'localhost' IDENTIFIED BY 'schedule';
-GRANT SELECT, INSERT, DELETE, UPDATE ON study_planner.* TO 'student_test'@'localhost';
+IF
+EXISTS DROP
+USER 'student_test'@'localhost';
+CREATE
+USER 'student_test'@'localhost' IDENTIFIED BY 'schedule';
+GRANT
+SELECT,
+INSERT
+,
+DELETE,
+UPDATE
+ON study_planner.* TO 'student_test'@'localhost';
 
