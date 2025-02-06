4fdac81b4381f9a0dd4445743f0ab27498583cf3
hilda <hilda.hermunen@gmail.com>
2025-02-06 09:59:39 +0200

    AddTaskController: filling choiceboxes with choices

--- src/main/java/controllers/AddTaskController.java
+++ src/main/java/controllers/AddTaskController.java
@@ -1,4 +1,55 @@
 package controllers;
 
-public class AddTaskController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddTaskController implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> taskChoiceBox;
+
+    private String[] tasks = {"Assignment", "Course", "Exam", "Study Session"};
+
+    @FXML
+    private ChoiceBox<String> startTimeChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> endTimeChoiceBox;
+
+    private String[] startTimes = {"6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"};
+
+    private String[] endTimes = {"7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"};
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button backButton;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        taskChoiceBox.getItems().addAll(tasks);
+        taskChoiceBox.setOnAction(this::getTask);
+        startTimeChoiceBox.getItems().addAll(startTimes);
+        endTimeChoiceBox.getItems().addAll(endTimes);
+    }
+
+    public void getTask(ActionEvent event){
+        String task = taskChoiceBox.getValue();
+    }
+
+    public void getStartTime(ActionEvent event){
+        String startTime = startTimeChoiceBox.getValue();
+    }
+
+    public void getEndTime(ActionEvent event){
+        String endTime = endTimeChoiceBox.getValue();
+    }
+
 }
