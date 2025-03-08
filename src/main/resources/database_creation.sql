824fa54d559d9a628a1f6a65f440578b0224a3ed
vevego <veera.ruotsalainen@metropolia.fi>
2025-03-08 22:46:45 +0200

    lisätty not nulleja

--- src/main/resources/database_creation.sql
+++ src/main/resources/database_creation.sql
@@ -6,53 +6,52 @@ USE study_planner;
 CREATE TABLE course
 (
     course_name VARCHAR(50) PRIMARY KEY,
-    instructor  VARCHAR(50),
-    start_date  DATE,
-    end_date    DATE
+    instructor  VARCHAR(50) NOT NULL ,
+    start_date  DATE NOT NULL ,
+    end_date    DATE NOT NULL ,
 );
 
 CREATE TABLE study_session
 (
     session_id  INT AUTO_INCREMENT PRIMARY KEY,
-    course_name VARCHAR(50),
-    title       VARCHAR(50),
+    course_name VARCHAR(50) NOT NULL,
+    title       VARCHAR(50) NOT NULL,
     description VARCHAR(100),
-    start_time  DATETIME,
-    end_time    DATETIME,
+    start_time  DATETIME NOT NULL,
+    end_time    DATETIME NOT NULL,
     FOREIGN KEY (course_name) REFERENCES course (course_name)
 );
 
 CREATE TABLE assignment
 (
     assignment_id INT AUTO_INCREMENT PRIMARY KEY,
-    course_name   VARCHAR(50),
-    title         VARCHAR(50),
+    course_name   VARCHAR(50) NOT NULL,
+    title         VARCHAR(50) NOT NULL,
     description   VARCHAR(100),
-    deadline      DATETIME,
-    status        VARCHAR(20),
+    deadline      DATETIME NOT NULL,
+    status        VARCHAR(20) NOT NULL,
     FOREIGN KEY (course_name) REFERENCES course (course_name)
 );
 
 CREATE TABLE exam
 (
     exam_id     INT AUTO_INCREMENT PRIMARY KEY,
-    course_name VARCHAR(50),
-    exam_date   DATETIME,
-    title       VARCHAR(50),
+    course_name VARCHAR(50) NOT NULL,
+    exam_date   DATETIME NOT NULL,
+    title       VARCHAR(50) NOT NULL,
     description VARCHAR(100),
-    location    VARCHAR(15),
+    location    VARCHAR(15) NOT NULL,
     FOREIGN KEY (course_name) REFERENCES course (course_name)
 );
 
 CREATE TABLE class_schedule
 (
     class_id    INT AUTO_INCREMENT PRIMARY KEY,
-    course_name VARCHAR(50),
-    location    VARCHAR(15),
+    course_name VARCHAR(50) NOT NULL,
+    location    VARCHAR(15) NOT NULL,
     description VARCHAR(100),
-    start_time  DATETIME,
-    end_time    DATETIME,
-    day_of_week VARCHAR(10),
+    start_time  DATETIME NOT NULL,
+    end_time    DATETIME NOT NULL,
     FOREIGN KEY (course_name) REFERENCES course (course_name)
 );
 
